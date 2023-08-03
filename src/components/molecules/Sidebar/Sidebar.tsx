e49074cde9b3e6b7b5375b7dff8a67b448129a3f
xypxzy <xypxzy@gmail.com>
2023-08-03 23:09:05 +0600

    add sidebar styles to css module

--- src/components/molecules/Sidebar/Sidebar.tsx
+++ src/components/molecules/Sidebar/Sidebar.tsx
@@ -1,16 +1,16 @@
 import {useContext} from "react";
 import {SidebarContext} from "../../../contexts/SidebarContext/SidebarContext.ts";
-import cls from './Sidebar.module.css'
 import {IoMdArrowForward} from "react-icons/io";
+import cls from './Sidebar.module.css'
 function Sidebar() {
     const {isOpen, handleClose} = useContext(SidebarContext);
     return (
         <div className={`${cls.sidebar} ${isOpen ? 'right-0' : '-right-full'}`}>
-            <div className={"flex justify-between items-center py-6 border-b"}>
-                <div className={"uppercase font-semibold text-sm"}>Shopify Bag (0)</div>
+            <div className={cls.wrapper}>
+                <div className={cls.header}>Shopify Bag (0)</div>
                 <div
                     onClick={handleClose}
-                    className={"w-8 h-8 cursor-pointer flex justify-center items-center"}>
+                    className={cls.backBtn}>
                     <IoMdArrowForward className={"text-xl"}/>
                 </div>
             </div>
